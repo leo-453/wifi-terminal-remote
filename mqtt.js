f0301e6b8c85b2f2a0dc59b29f874517407a7a18
leo-453 <leoart453@gmail.com>
2026-05-21 11:01:29 +0200

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -251,7 +251,8 @@ function publishMessage() {
         return;
     }
 
-    let text = remoteClientID + ":" + cmd;   // <=== FORMATO CORRETTO
+    //let text = remoteClientID + ":" + cmd;   // <=== FORMATO CORRETTO
+        let text = cmd;  
 
     console.log("TX →", topic_sub, ":", text);
     client.publish(topic_sub, text);
